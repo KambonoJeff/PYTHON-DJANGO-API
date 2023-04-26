1ad1f0cb85fdfaee75b324dde8972227fbd9d55d
KAMBONO <kambonoj@gmail.com>
2023-04-26 15:03:39 +0300

    Added The React Frontend UI folder

---
+++ React-Front/UI/electronics_e-commerce/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        React Starter UI Template For Electronic Ecommerce web based app
+        <br/>
+        <br/>
+        BY KambonoJeff & SalathielWekesa 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
